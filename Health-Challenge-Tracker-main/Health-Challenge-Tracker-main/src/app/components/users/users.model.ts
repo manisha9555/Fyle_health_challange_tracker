4f96d335cd800fa3177d3597ceabbf88773d1de5
manisha9555 <115913147+manisha9555@users.noreply.github.com>
2024-07-29 00:17:34 +0530

    Delete Health-Challenge-Tracker-main/Health-Challenge-Tracker-main directory

--- Health-Challenge-Tracker-main/Health-Challenge-Tracker-main/src/app/components/users/users.model.ts
+++
@@ -1,9 +0,0 @@
-import { Workout } from '@/app/components/add-user/add-user.model';
-
-export interface User {
-  id: number;
-  name: string;
-  workouts: Workout[];
-  totalWorkouts: number;
-  totalMinutes: number;
-}
